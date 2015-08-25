6fe7aaa2e92155a1cbaaea8ae85b72de3fc5c6f5
Andy Walz <andywalz@gmail.com>
2015-08-25 14:52:33 -0500

    Removed mailto

--- js/components/appIssues/controller/appIssuesController.js
+++ js/components/appIssues/controller/appIssuesController.js
@@ -16,14 +16,14 @@ define([
 
     buildLink: function(){
 
-      var emailSubject = encodeURI('I found an issue or bug with the Solar app');
-      var emailBody = encodeURI('The issue or bug can be desribed as:\n' + $('#appIssuesDescription').val());
+      // var emailSubject = encodeURI('I found an issue or bug with the Solar app');
+      // var emailBody = encodeURI('The issue or bug can be desribed as:\n' + $('#appIssuesDescription').val());
 
-      var emailSignature = encodeURI('\n\nName: ' + $('#appIssuesName').val() + '\nEmail: ' + $('#appIssuesEmail').val());
+      // var emailSignature = encodeURI('\n\nName: ' + $('#appIssuesName').val() + '\nEmail: ' + $('#appIssuesEmail').val());
 
-      var link = 'mailto:' + config.emailAddress + '?subject=' + emailSubject + '&body=' + emailBody + emailSignature;
+      // var link = 'mailto:' + config.emailAddress + '?subject=' + emailSubject + '&body=' + emailBody + emailSignature;
 
-      $('#appIssuesSubmit').attr('href', link);
+      // $('#appIssuesSubmit').attr('href', link);
     }
 
   };
