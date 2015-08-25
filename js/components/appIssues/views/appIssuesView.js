4f631cb5eb8b7976b68e6451a1ac7bdcf259db18
Chris Martin <cmartin616@gmail.com>
2015-08-24 20:37:02 -0400

    Wired up buttons but PHP mailer doesn't seem to work

--- js/components/appIssues/views/appIssuesView.js
+++ js/components/appIssues/views/appIssuesView.js
@@ -40,11 +40,28 @@ define([
 
       initComponents: function() {
         $('.closeSplash').on('click', function(){
-          $('.appIssuesModal').modal('hide');
+          $('.dataIssuesModal').modal('hide');
         });
 
-        $('#appIssuesDescription, #appIssuesName, #appIssuesEmail').on('input', function(){
-          appIssuesController.buildLink();
+        /* I don't remember writing this.  Did you? */
+        // $('#appIssuesDescription, #appIssuesName, #appIssuesEmail').on('input', function(){
+        //   appIssuesController.buildLink();
+        // });
+
+        $('.dataIssuesSubmit').on('click', function(){
+          var emailData = {
+                            to: config.appEmail,
+                            to_name: '',
+                            from: $('#dataIssuesEmail').val(),
+                            from_name: $('#dataIssuesName').val(),
+                            subject: 'Solar Suitability Data Issue',
+                            body: 'An error was found with the Solar Suitability data.  Please see below for a description:<br><br>' + $('#dataIssuesDescription').val() + ' at ' + $('#dataIssuesLocation').val(),
+                            skey: 'Vdb2PwCgMQsEVV3jWfLvqEMLeXchevqq'
+                          };
+
+          $.post('api/email.php', emailData, function(data){
+            $('.dataIssuesModal').modal('hide');
+          });
         });
       }
 
