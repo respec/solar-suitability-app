e86d989b68a0e5e54c38b1e326c31ef5f4577c23
Chris Martin <cmartin616@gmail.com>
2015-08-24 20:18:30 -0400

    Minor jshint fixes

--- js/components/email/views/emailView.js
+++ js/components/email/views/emailView.js
@@ -47,16 +47,16 @@ define([
         $('.emailSubmit').on('click', function(){
           var emailLink = emailController.buildEmailLink();
           var emailData = {
-                              to: $('#recipEmail').val(),
-                              to_name: '',
-                              from: $('#emailEmail').val(), 
-                              from_name: $('#emailName').val(), 
-                              subject: 'Solar Suitability Report',
-                              body: '<p>A Solar Suitability Analysis Report has been shared with you. Click the link below to view:</p><p><a href="' + emailLink + '">' + emailLink + '</a></p><p>' + $('#customMsg').val() + '</p>',
-                              skey: 'Vdb2PwCgMQsEVV3jWfLvqEMLeXchevqq'
-                            };
+                            to: $('#recipEmail').val(),
+                            to_name: '',
+                            from: $('#emailEmail').val(),
+                            from_name: $('#emailName').val(),
+                            subject: 'Solar Suitability Report',
+                            body: '<p>A Solar Suitability Analysis Report has been shared with you. Click the link below to view:</p><p><a href="' + emailLink + '">' + emailLink + '</a></p><p>' + $('#customMsg').val() + '</p>',
+                            skey: 'Vdb2PwCgMQsEVV3jWfLvqEMLeXchevqq'
+                          };
 
-          $.post("api/email.php", emailData, function(data){
+          $.post('api/email.php', emailData, function(data){
             $('.emailModal').modal('hide');
           });
         });
