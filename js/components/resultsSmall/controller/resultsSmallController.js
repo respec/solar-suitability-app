d7b1667827e99ef27b74f799e9c5d736fb4b6c42
Chris Martin <cmartin616@gmail.com>
2015-10-02 19:28:40 -0400

    Moved buildTable to reportController where it belongs

--- js/components/resultsSmall/controller/resultsSmallController.js
+++ js/components/resultsSmall/controller/resultsSmallController.js
@@ -21,23 +21,6 @@ define([
       $('#resultsSmall').hide();
     },
 
-    buildTable: function(el, data, values, ref){
-      var $table = $(el);
-      _.each(ref, function(mon){
-        var shortMonth = mon.abbr;
-        var longMonth = mon.full;
-        $table.find('tbody')
-          .append($('<tr>')
-            .append($('<td style="width:50%">')
-              .text(longMonth)
-            )
-            .append($('<td>')
-              .text(data[shortMonth][values].toFixed(2))
-            )
-          );
-      });
-    },
-
     buildLink: function(){
       var url = config.appDomain + '/index.html?lat=' + app.query.latLngPt.y + '%26long=' + app.query.latLngPt.x;
 
