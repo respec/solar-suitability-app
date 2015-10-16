cd4f4bb4cbdfe647ce592c9f95c1b5fbbe8026be
Andy Walz <andywalz@gmail.com>
2015-10-16 12:55:05 -0500

    Email reply-to and invalid address fix

--- api/email.php
+++ api/email.php
@@ -2,35 +2,51 @@
 
 if($_POST['skey'] == "Vdb2PwCgMQsEVV3jWfLvqEMLeXchevqq") {
 
-	//$to = "solarp@umn.edu";
 	$to = $_POST['to'];//"andywalz@gmail.com";
-	$to_name = $_POST['to_name'];//"Bad Data Czar";
-	$subject = $_POST['subject'];//"Bad Data Notification";
-	//$body = "<br>This location may need to be reprocessed: <a href='http://solar.maps.umn.edu/app/index.html?lat=" . $_REQUEST['lat'] . "&long=" . htmlentities($_REQUEST['long']) ."'>" . $_REQUEST['lat'] . ", " . $_REQUEST['long'] . "</a><br><br>" . $_REQUEST['notes']. "<br><br>Submitted by: ".$_REQUEST['name']." - ".$_REQUEST['email']."<br><br>";
-	$body = $_POST['body'];//"Test 123"
-	$replytoaddress = $_REQUEST['from_email'];
-	$fromname = $_REQUEST['from_name'];
 
-	$result = send_email($to, $to_name, $body, $subject, $fromaddress="mnsolarsuitability@gmail.com", $fromname, $replytoaddress);	
-
-	if($result == "Message sent!") {
+	if( isBadEmail($to) ) {
+		//Email address is bad
 		if (version_compare(PHP_VERSION, '5.2.0') >= 0) {
-			$response_arr = array('success' => $result);
+			$response_arr = array('error' => $to . ' is not a valid email address.');
 			$sJson = json_encode( $response_arr );
 		} else {
 			// PHP Version does not support json_
-			$sJson = '{"success":"'.$result.'"}';
+			$sJson = '{"error":"'.$to . ' is not a valid email address."}';
 		}
 	} else {
-		// Message failed to send
-		http_response_code(400);
 
-		if (version_compare(PHP_VERSION, '5.2.0') >= 0) {
-			$response_arr = array('error' => $result);
-			$sJson = json_encode( $response_arr );
+		//Email is valid, attempt to send
+		$to_name = $_POST['to_name'];//"Bad Data Czar";
+		$subject = $_POST['subject'];//"Bad Data Notification";
+		
+		$body = $_POST['body'];//"Test 123"
+		$replytoaddress = $_POST['from'];
+		$replytoname = "";
+		$fromname = $_POST['from_name'];
+		$fromaddress="mnsolarsuitability@gmail.com";
+
+		$result = send_email($to, $to_name, $body, $subject, $fromaddress, $fromname, $replytoaddress, $replytoname);
+		
+		if($result == "Message sent!") {
+			if (version_compare(PHP_VERSION, '5.2.0') >= 0) {
+				$response_arr = array('success' => $result);
+				$sJson = json_encode( $response_arr );
+			} else {
+				// PHP Version does not support json_
+				$sJson = '{"success":"'.$result.'"}';
+			}
 		} else {
-			// PHP Version does not support json_
-			$sJson = '{"error":"'.$result.'"}';
+			
+			// Message failed to send
+			//http_response_code(400);
+
+			if (version_compare(PHP_VERSION, '5.2.0') >= 0) {
+				$response_arr = array('error' => $result);
+				$sJson = json_encode( $response_arr );
+			} else {
+				// PHP Version does not support json_
+				$sJson = '{"error":"'.$result.'"}';
+			}
 		}
 	}
 	
