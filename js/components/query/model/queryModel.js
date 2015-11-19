6e4772a898caecd895c1c1da17fb4b0ce5de704c
Chris Martin <cmartin616@gmail.com>
2015-11-18 21:33:03 -0500

    cleanup (remove unused functions)

--- js/components/query/model/queryModel.js
+++ js/components/query/model/queryModel.js
@@ -9,19 +9,6 @@ function(
       var QueryModel = Backbone.Model.extend({
         defaults: {},
 
-        setValue: function(key,val){
-          var newVal = {};
-          newVal[key] = val;
-          // var newVal = {
-          //   key: val
-          // };
-          this.set(newVal);
-        },
-
-        test: function(val){
-          this.set({'quality': val});
-        }
-
       });
     return QueryModel;
   });
