ae042657948962bcf7ac0e78d60aeab298dda06d
Andy Walz <andywalz@gmail.com>
2016-02-10 23:59:10 -0600

    Updated gpTool to include ideal results

--- gpTool/SolarIdealQuery.py
+++ gpTool/SolarIdealQuery.py
@@ -1,4 +1,4 @@
-import arcpy
+import sys, os, arcpy
 from arcpy import env
 from arcpy.sa import *
 
@@ -7,15 +7,15 @@ arcpy.CheckOutExtension("spatial")
 arcpy.env.overwriteOutput = True
 
 ### Parameters supplied by the map click query component in the app
-pointX = arcpy.GetParameter(0)   # pointx = -94.6479228437483
-pointY = arcpy.GetParameter(1)		# pointy = 45.6275948974094
-filename = arcpy.GetParameter(2)	# filename = "3526-11-44_1231.img"
+##pointX = arcpy.GetParameter(0)   # pointx = -94.6479228437483
+##pointY = arcpy.GetParameter(1)		# pointy = 45.6275948974094
+##filename = arcpy.GetParameter(2)	# filename = "3526-11-44_1231.img"
 
 ### Sample data for testing
-#pointX = -94.6479228437483
-#pointY = 45.6275948974094
-#filename = "3526-11-44_1231.img"
-
+pointX = -94.6479228437483
+pointY = 45.6275948974094
+filename = "3526-11-44_1231.img"
+#import pdb; pdb.set_trace()
 ### New q250k tile scheme
 quad = filename[:4]
 ras = "\\\\files.umn.edu\\US\\GIS\\U-Spatial\\SolarResourceData\\MN_DSM\\DSM_Tiles_q250k\\q" + quad + "\\" + filename
@@ -23,15 +23,22 @@ ras = "\\\\files.umn.edu\\US\\GIS\\U-Spatial\\SolarResourceData\\MN_DSM\\DSM_Til
 arcpy.env.addOutputsToMap = False
 #arcpy.env.workspace = r'C:\Temp\temp.gdb'
 
-arcpy.env.workspace = "\\\\files.umn.edu\\US\\GIS\\U-Spatial\\SolarResourceData\\gpTool\\SolarPointQuery.gdb"
+ws = "\\\\files.umn.edu\\US\\GIS\\U-Spatial\\SolarResourceData\\gpTool\\solarresults.gdb"
 
+### Insolation and Duration tables
+output = os.path.join(ws,"actualInsolation")
+output_hr = os.path.join(ws,"sunDuration")
+
+### Ideal shading tables
 in_name = 'inTable'
-out_name = 'outTable'
-output = arcpy.env.workspace + "\\" + out_name
-inTable = arcpy.env.workspace + "\\" + in_name
+out_name = 'idealTotal'
+outputIdeal = os.path.join(ws,out_name)
+inTable = os.path.join(ws,in_name)
+outputDirect = os.path.join(ws,"idealDirect")
+#outputDiffuse = os.path.join(ws,"idealDiffuse")
 
 ### Create a Table to Store input point and ideal tilt/azimuth
-arcpy.CreateTable_management(arcpy.env.workspace, in_name)
+arcpy.CreateTable_management(ws, in_name)
 
 ### Add fields
 arcpy.AddField_management(inTable, "x", "FLOAT")
@@ -58,23 +65,34 @@ cursor = arcpy.da.InsertCursor(inTable, fieldsToUpdate)
 cursor.insertRow((x,y,45,180))
 del cursor
 
-##point = arcpy.Point(pointX, pointY)
-##ptGeometry = arcpy.PointGeometry(point)
-
 timeSetting = arcpy.sa.TimeWholeYear(2014)
 #timeSetting = arcpy.sa.TimeSpecialDays()
 
-#arcpy.sa.PointsSolarRadiation(ras,ptGeometry,output,time_configuration=timeSetting,each_interval="INTERVAL",out_direct_duration_features=output_hr)
-arcpy.sa.PointsSolarRadiation(ras,inTable,output,time_configuration=timeSetting,each_interval="INTERVAL",slope_aspect_input_type="FROM_POINTS_TABLE")
+arcpy.sa.PointsSolarRadiation(ras,ptGeometry,output,time_configuration=timeSetting,each_interval="INTERVAL",out_direct_duration_features=output_hr)
+arcpy.sa.PointsSolarRadiation(ras,inTable,outputIdeal,time_configuration=timeSetting,each_interval="INTERVAL",slope_aspect_input_type="FROM_POINTS_TABLE",out_direct_radiation_features=outputDirect)
 
-#row = arcpy.SearchCursor(output).next()
+row = arcpy.SearchCursor(output).next()
+result = ""
+for i in range(0,12):
+    id = "T"+str(i)
+    result = str(result) + str(row.getValue(id)) + "\n"
 
-#result = ""
+row = arcpy.SearchCursor(output_hr).next()
+result_hr = ""
+for i in range(0,12):
+    id = "T"+str(i)
+    result_hr = str(result_hr) + str(row.getValue(id)) + "\n"
 
-#for i in range(0,12):
-# id = "T"+str(i)
-# result = str(result) + str(row.getValue(id)) + "\n"
+row = arcpy.SearchCursor(outputDirect).next()
+result_ideal = ""
+for i in range(0,12):
+    id = "T"+str(i)
+    result_ideal = str(result_ideal) + str(row.getValue(id)) + "\n"
 
+arcpy.SetParameter(3,result)
 #print result
-
+arcpy.SetParameter(4,result_hr)
+#print result_hr
+arcpy.SetParameter(5,result_ideal)
+#print result_ideal
 
